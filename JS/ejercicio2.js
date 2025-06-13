84caf4c488841ea0e1a9786aee6da48dcbb29006
ignaciolp <ignaciolp@lepa.com.ar>
2025-06-13 18:47:36 -0300

    termino ejercicio 2

---
+++ JS/ejercicio2.js
@@ -0,0 +1,23 @@
+const nota = parseInt(prompt(`Ingrese su nota`));
+if (nota >= 0 && nota <= 2) {
+  alert(`Muy deficiente`);
+}
+if (nota >= 3 && nota <= 4) {
+  alert(`Insuficiente`);
+}
+if (nota >= 5 && nota <= 6) {
+  alert(`Suficiente`);
+}
+if (nota === 7) {
+  alert(`Bien`);
+}
+if (nota >= 8 && nota <= 9) {
+  alert(`Notable`);
+}
+if (nota === 10) {
+  alert(`Sobresaliente`);
+}
+
+if (nota < 0 || nota > 10) {
+  alert(`Introduce un numero valido`);
+}
